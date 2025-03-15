369b3029c1ca892269bea685e5e528e1063b6352
Emanuelly <emanuellygomes.manu2004@gmail.com>
2025-03-14 21:24:30 -0300

    add lista

--- src/components/ListaTarefas/ListaTarefas.jsx
+++ src/components/ListaTarefas/ListaTarefas.jsx
@@ -1,10 +1,17 @@
+import style from './ListaTarefas.module.css'
+import { ListaTarefasItem } from './ListaTarefasItem/'
+
 const ListaTarefas = () => {
+    const tarefas = [
+        {id: 1, nome: 'Item 1'},
+        {id: 2, nome: 'Item 2'},
+        {id: 3, nome: 'Item 3'}
+    ] 
+
     return (
-        <ul>
-            <li> Item 1 </li>
-            <li> Item 2 </li>
-            <li> Item 3 </li>
-        </ul>
+        <lu className={style.ListaTarefas}>
+            {tarefas.map(item => <ListaTarefasItem key={item.id} nome={item.nome} />)}
+        </lu>
     )
 }
 
--- src/components/ListaTarefas/ListaTarefasItem/index.js
+++ src/components/ListaTarefas/ListaTarefasItem/index.js
@@ -1,2 +1,1 @@
-export * from './ListaTarefas'
 export * from './ListaTarefasItem'
